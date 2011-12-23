d845487d657bf79b693681525b105bf17245565c
D.K <lazyklimm@gmail.com>
2011-12-23 18:45:42 +0400

    multiple audio player control script :D

---
+++ apctl.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+cmd=$1
+
+deadbeef_run ()
+{
+    which deadbeef >/dev/null && pgrep deadbeef >/dev/null 
+}
+
+deadbeef_np ()
+{
+    deadbeef --nowplaying %a 2>/dev/null | grep -qv ^nothing
+}
+
+deadbeef_next ()
+{
+    deadbeef --next
+}
+
+deadbeef_prev ()
+{
+    deadbeef --prev
+}
+
+deadbeef_toggle ()
+{
+    deadbeef --play-pause
+}
+
+xmms2_run ()
+{
+    which nyxmms2 >/dev/null && pgrep xmms2d >/dev/null
+}
+
+xmms2_np ()
+{
+    nyxmms2 current | grep -q ^Playing
+}
+
+xmms2_next ()
+{
+    nyxmms2 next
+}
+
+xmms2_prev ()
+{
+    nyxmms2 prev
+}
+
+xmms2_toggle ()
+{
+    nyxmms2 toggle
+}
+
+for pl in xmms2 deadbeef
+do
+    ${pl}_run && ${pl}_${cmd}
+done
