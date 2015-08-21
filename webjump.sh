06cedc2e781c4834faf696cc059abdbb3dfe1191
DK <beerandhot@gmail.com>
2015-08-21 13:43:02 +0300

    something new

--- webjump.sh
+++ webjump.sh
@@ -9,7 +9,6 @@ ldd /usr/bin/dmenu | grep -q libXft && \
     FONT="-*-terminus-medium-*-*-*-18-*-*-*-*-*-iso10646-*"
 
 DMENU="dmenu -fn $FONT -nb $NB -nf $NF -sb $SB -sf $SF"
-#DMENU="xwinmosaic -rp -S"
 test -n "$1" && browser=$1 || exit 1
 
 SEARCH="https://encrypted.google.com/search?ie=utf-8&oe=utf-8&aq=t&q="
@@ -24,7 +23,7 @@ case $browser in
         ;;
     conkeror)
         ROOT=$HOME/.conkeror.mozdev.org/conkeror
-        DB=$ROOT/`sed -n 's/Path=\(.*\)/\1/p' $ROOT/profiles.ini`/places.sqlite
+        DB=$ROOT/`sed -n 's/Path=\(.*\)/\1/p' $ROOT/profiles.ini `/places.sqlite
         TABLES="moz_places"
         FIELDS="title,url"
         ORDER="last_visit_date"
@@ -52,7 +51,7 @@ case $browser in
         ;;
 esac
 
-urlstr=`sqlite3 $DB 'select '$FIELDS' from '$TABLES' order by '$ORDER' desc' | $DMENU -i -l 15`
+urlstr=`sqlite3 "$DB" 'select '$FIELDS' from '$TABLES' order by '$ORDER' desc' | $DMENU -i -l 15`
 #urlstr=`sqlite3 $DB 'select '$FIELDS' from '$TABLES | $DMENU`
 test -z "$urlstr" && exit 1
 echo "$urlstr" | grep -q \| && url=`echo $urlstr | cut -d \| -f 2` || url=$SEARCH$urlstr
